4fe9258fa72e2347db0e4f914e787af0cd74a86f
Nikita Chernyi <developer.nikus@gmail.com>
2016-11-24 14:08:40 +0200

    Added 60 sec sleep before db import

--- docker/init.sh
+++ docker/init.sh
@@ -5,7 +5,8 @@ echo -n "${HTTP_AUTH_USER}:" >> /etc/nginx/htpasswd
 openssl passwd -apr1 -salt "$HTTP_AUTH_SALT" "$HTTP_AUTH_PASSWORD" >> /etc/nginx/htpasswd
 
 /usr/bin/mysqld_safe &
-sleep 10
+echo -e "\033[0;31mSLEEP 60 seconds. Don't worry, all ok, we just need wait mysql server to start.\033[0m"
+sleep 60
 mysqladmin -u root password "$MYSQL_PASSWORD"
 mysql -u root -p$MYSQL_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE;"
 mysql -u root -p$MYSQL_PASSWORD $MYSQL_DATABASE < /tmp/init.sql
